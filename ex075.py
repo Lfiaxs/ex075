5b0752a36d498561c0ab18f75f1c3742bc7f3caf
Lucas Fiais <lucas.fiais@hotmail.com>
2024-09-19 10:03:39 -0300

    Add files via upload

---
+++ ex075.py
@@ -0,0 +1,15 @@
+núm = (int(input('Digite um número: ')),
+      int(input('Digite um outro número: ')),
+      int(input('Digite um mais um número: ')),
+      int(input('Digite o último número: ')))
+print(f'Você digitou osvalores{núm}')
+print(f'O valor 9 apareceu {núm.count(9)} vezes.')
+if 3 in núm:
+    print(f'O valor 3 apareceu na {núm.index(3)+1}ª posição.')
+else:
+    print('O valor 3 não foi digitado em nenhuma posição.')
+print(f'Os valores pares digitados foram', end=' ')
+for n in núm:
+    if n % 2 == 0:
+        print(n, end=' ')
+
